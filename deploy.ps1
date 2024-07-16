12405a1cfdfe9dfc0b7c629a7ae435acc577b7d1
Raphael <raphael@none.com>
2024-07-16 21:36:03 +0200

    wip

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
 ﻿docker build -t poll .
-docker tag poll registry.www.areya.fr/poll:v1
-docker push registry.www.areya.fr/poll:v1
+docker tag poll registry.w.thera-engineering.com/poll:v1
+docker push registry.w.thera-engineering.com/poll:v1
